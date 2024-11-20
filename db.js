15310c8def29562c082428012f4de38a5f1cbbed
IvonneBell <agonzale4_22@alu.uabcs.mx>
2024-11-20 11:31:30 -0700

    Firs commit

---
+++ db.js
@@ -0,0 +1,26 @@
+const  mysql =require('mysql2/promise');
+
+async function connect() {
+    try{
+        const HOST='localhost';
+        const PORT =3306;
+        const USER = 'root';
+        const PASS ='BoomerCff196';
+        const DATABASE ='BANCA_MOVIL';
+
+        const conn = await mysql.createConnection({
+            'host': HOST,
+            'port': PORT,
+            'user': USER,
+            'database': DATABASE
+        });
+        console.log('conexion creada');
+        return conn;
+
+    } catch(err){
+        console.log('ocurrio un error: '+ err);
+        throw err;
+    }
+}
+
+module.exports = connect;
---
+++ index.js
@@ -0,0 +1,115 @@
+const express = require('express'); //FRAMEWORK
+const app = express();
+const PORT = 3000;
+const connect=require('./db');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/users', async(req, res)=>{
+    let db;
+    try {
+
+        db = await connect();
+        const query ='SELECT * FROM usuarios';
+        const [row] =await db.execute(query);
+        console.log(row);
+        res.json({
+            status:200, 
+            users: row
+        });
+    }catch(err){
+        console.log(err);
+    }
+});
+
+app.post('/users', async(req, res)=>{
+    let db;
+    try {
+        const {email,nombre}=req.body;
+        db = await connect();
+        const query =`INSERT INTO usuarios(nombre, email)VALUES('${nombre}','${email}')`;
+        const [row] =await db.execute(query);
+        console.log(row);
+        res.json({
+            status:200, 
+            users: row
+        });
+    }catch(err){
+        console.log(err);
+    }
+});
+
+app.get('/users/:email', async (req, res)=>{
+    const email =req.params.email;
+    let db;
+    try {
+        db = await connect();
+        const query = 'SELECT * FROM usuarios WHERE email = ?';
+        const [row]= await db.execute(query, [email]);
+        console.log(row);
+        res.json({
+            'status':200,
+            'users':row
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.delete('/users/:email', async (req,res)=>{
+    const email =req.params.email;
+    let db;
+    try {
+        db = await connect();
+        const query = 'DELETE FROM usuarios WHERE email = ?';
+        const [row]= await db.execute(query, [email]);
+        if(row.affectedRows == 0) {
+            res.json({
+                'users': [],
+                'status': 404,
+                'msg': 'Email no encontrado',
+            })
+        }else{
+            res.json({
+                'status':200,
+                'users':[]
+            });
+
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.put('/users/:email', async (req, res)=>{
+    const email = req.params.email;
+    const {nombre}= req.body;
+
+    try {
+        db = await connect();
+        const query = 'UPDATE usuarios SET nombre = ? where email = ?';
+        const [row]= await db.execute(query, [nombre, email]);
+        if(row.affectedRows == 0) {
+            res.json({
+                'users': [],
+                'status': 404,
+                'msg': 'Email no encontrado',
+            })
+        }else{
+            res.json({
+                'status':200,
+                'users':[]
+            });
+
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`Escuchando por el puerto ${PORT}`);
+});
